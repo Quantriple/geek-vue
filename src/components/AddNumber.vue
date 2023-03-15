40082e8d1912a1f21df933c30f4af23246cf71bb
TripleQ <15258551103@163.com>
2023-03-15 15:44:15 +0800

    累加器颜色随机变化

--- src/components/AddNumber.vue
+++ src/components/AddNumber.vue
@@ -9,12 +9,16 @@
 import {ref} from "vue";
 
 let count=ref(0);
+let color =ref("blue");
 function add() {
   count.value--;
+  color.value =Math.random()>0.5?"blue":"red";
 }
   
 </script>
 
 <style scoped>
-
+h1 {
+  color: v-bind(color)
+}
 </style>
